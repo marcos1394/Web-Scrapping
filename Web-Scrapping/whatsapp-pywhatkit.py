3058493ee2aa3be11134ddbee67e62e09315517b
Marcos Sandoval <marcosbancaprepa@gmail.com>
2022-06-25 21:40:03 -0600

    web-scrapping

--- Web-Scrapping/whatsapp-pywhatkit.py
+++ Web-Scrapping/whatsapp-pywhatkit.py
@@ -2,4 +2,8 @@ import pywhatkit
 
 numero_celular = input("Ingresa el numero de celular: ")
 
-pywhatkit.sendwhatmsg(numero_celular,"Test", 8, 38)
+pywhatkit.sendwhatmsg(numero_celular,"Test", 8, 39)
+
+
+id_grupo = input("ingresa el id del grupo: ")
+pywhatkit.sendwhatmsg_to_group(id_grupo, 7, 22)
